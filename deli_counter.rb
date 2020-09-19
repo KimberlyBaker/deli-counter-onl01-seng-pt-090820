8c623a7afe87a9f25cf4237eb62fd7469e3ecd09
Kimberly Baker <bakerk2010@gmail.com>
2020-09-19 17:18:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,12 +10,12 @@ take_a_number(katz_deli, "Avi")
 take_a_number(katz_deli, "Spencer")
 
 def line(katz_deli)
-  if katz_deli.empty?
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     message = "The line is currently:"
     katz_deli.each_with_index do |value, index|
-    message << " #{index+1}. #{value}"
+    message << " #{index.to_i+1}. #{value}"
     end
     puts message
   end
